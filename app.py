9360419b8b04f21e0eab493a02ade8b18dc3ff54
madhurchouhan01 <madhurchouhan02@gmail.com>
2024-04-12 17:16:08 +0530

    Added 'Flask' as Backend

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify,request,Response,redirect,render_template
+import os
+import model
+app = Flask(__name__,  static_url_path='/static')
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return 'No file part'
+        
+        img = request.files['image']
+
+        if img.filename == '':
+            return 'No selected file'
+        
+        basepath = os.path.dirname(__file__)
+        filepath = os.path.join(basepath, 'uploads',img.filename)
+        img.save(filepath)
+        result = model.process_image(filepath)
+        return render_template('home.html', result=result)
+        
+    return render_template('home.html')
+
+
+if __name__ == 'main':
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,48 +1,19 @@
-import threading
 import cv2
 from deepface import DeepFace
 
+def process_image(filepath):
+    ref_img = cv2.imread(filepath)
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+    ret , frame = cap.read()
 
-counter = 0
-face_match = False
-ref_img = cv2.imread('refer.jpg')
-
-
-def check_face(frame):
-  global face_match
-  try:
-    if DeepFace.verify(frame,ref_img.copy())['verified']:
-      face_match = True
-    else:
-      face_match = False
-  except ValueError:
-    face_match = False
-
-while True:
-  ret, frame = cap.read()
-  if ret:
-    if counter%30 == 0:
-      try:
-        thread = threading.Thread(target = check_face, args=(frame.copy(),))
-        thread.start()
-        thread.join()
-      except ValueError:
-        pass
-      counter += 1
-    if face_match:
-      cv2.putText(frame, "MATCH!",(20,450),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
-    else:
-      cv2.putText(frame, "Not MATCH!",(20,450),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
-    cv2.imshow("video", frame)
-
-  key = cv2.waitKey(1)
-  if key == ord('q'):
-    break
-cv2.destroyAllWindows()
-
-
+    try:
+        if DeepFace.verify(frame, ref_img.copy())['verified']:
+            return "MATCH!"
+        else:
+            return "Not MATCH!"
+    except ValueError:
+        return "Error processing image"
